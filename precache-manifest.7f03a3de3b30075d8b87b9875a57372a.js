fadea143b9b6dab73d51c2ac421eba88da419f35
huda <you@example.com>
2020-03-18 16:32:58 +0300

    Updates

---
+++ precache-manifest.7f03a3de3b30075d8b87b9875a57372a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f02cc9b7b1337e7f7b6dedc7889f03",
+    "url": "/SchoolApp_Frontend/index.html"
+  },
+  {
+    "revision": "8d8ec182d68cb7dae1ca",
+    "url": "/SchoolApp_Frontend/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "944fe2cf5695d47619a1",
+    "url": "/SchoolApp_Frontend/static/css/main.7e06e2bd.chunk.css"
+  },
+  {
+    "revision": "8d8ec182d68cb7dae1ca",
+    "url": "/SchoolApp_Frontend/static/js/2.61e02abd.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/SchoolApp_Frontend/static/js/2.61e02abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "944fe2cf5695d47619a1",
+    "url": "/SchoolApp_Frontend/static/js/main.098b7e24.chunk.js"
+  },
+  {
+    "revision": "b2c5ce517567ca1dd3d7",
+    "url": "/SchoolApp_Frontend/static/js/runtime-main.db8c548e.js"
+  }
+]);
